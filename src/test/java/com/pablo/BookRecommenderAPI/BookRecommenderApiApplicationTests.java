bef00f1270a7822ab174fd4ed39414edf1461b11
PabloHuerres <p.huerres01@gmail.com>
2024-11-09 17:24:06 +0100

    V.0

---
+++ src/test/java/com/pablo/BookRecommenderAPI/BookRecommenderApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pablo.BookRecommenderAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookRecommenderApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
